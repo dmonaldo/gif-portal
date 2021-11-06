644c2b00b420395d0d7251dedb6064e525a8efc2
Dante Monaldo <dante.monaldo@gmail.com>
2021-11-06 16:25:54 -0700

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -8,6 +8,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -21,6 +23,7 @@ const App = () => {
             'Connected with Public Key:',
             response.publicKey.toString()
           )
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Solana object not found. Get a Phantom wallet 👻');
@@ -31,7 +34,13 @@ const App = () => {
   }
 
   const connectWallet = async () => {
-    
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
   };
 
   const renderNotConnectedContainer = () => (
@@ -51,13 +60,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
